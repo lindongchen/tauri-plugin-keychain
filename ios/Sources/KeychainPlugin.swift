31169a7a9c4cd1ee3a704a7e4411b46f2464e251
DongChen Lin <393227948@qq.com>
2024-11-17 21:19:59 +0800

    impl swift

--- ios/Sources/KeychainPlugin.swift
+++ ios/Sources/KeychainPlugin.swift
@@ -1,10 +1,10 @@
 import SwiftRs
 import Tauri
-import UIKit
 import WebKit
 import ObjectiveC
 import Foundation
 import UniformTypeIdentifiers
+import Security
 
 class KeychainArgs: Decodable {
   let key: String
@@ -17,18 +17,58 @@ struct KeychainResponse: Codable {
 class KeychainPlugin: Plugin {
   @objc public func getItem(_ invoke: Invoke) throws {
     let args = try invoke.parseArgs(KeychainArgs.self)
-		//args.key
-		let resp = KeychainResponse(password: "aaa")
+		let key = args.key
+		let query = [
+				kSecClass: kSecClassGenericPassword,
+				kSecAttrAccount: key,
+				kSecReturnData: kCFBooleanTrue!,
+				kSecMatchLimit: kSecMatchLimitOne
+		] as CFDictionary
+		
+		var data: AnyObject?
+		let status = SecItemCopyMatching(query, &data)
+		
+		guard status == errSecSuccess, let resultData = data as? Data else {
+				return nil
+		}
+		
+		let password = String(data: resultData, encoding: .utf8)
+						
+		let resp = KeychainResponse(password: password)
 		invoke.resolve(resp)
   }
 	
 	@objc public func saveItem(_ invoke: Invoke) throws {
 	  let args = try invoke.parseArgs(KeychainArgs.self)
-		invoke.resolve(true)
+		let key = args.key
+		let value = args.password ?? ""
+		guard let data = value.data(using: .utf8) else { return false }
+		        
+		let query = [
+				kSecClass: kSecClassGenericPassword,
+				kSecAttrAccount: key
+		] as CFDictionary
+		SecItemDelete(query)
+		
+		let attributes = [
+				kSecClass: kSecClassGenericPassword,
+				kSecAttrAccount: key,
+				kSecValueData: data
+		] as CFDictionary
+		let status = SecItemAdd(attributes, nil)
+		
+		invoke.resolve(status == errSecSuccess)
 	}
 	@objc public func removeItem(_ invoke: Invoke) throws {
 	  let args = try invoke.parseArgs(KeychainArgs.self)
-		invoke.resolve(true)
+		let key = args.key
+		let query = [
+				kSecClass: kSecClassGenericPassword,
+				kSecAttrAccount: key
+		] as CFDictionary
+		
+		let status = SecItemDelete(query)
+		invoke.resolve(status == errSecSuccess)
 	}
 }
 
