646c830ed789ffda30140f82cb12f1f7cbd9914a
DongChen Lin <393227948@qq.com>
2024-11-18 15:40:49 +0800

    update readme

--- ios/Sources/KeychainPlugin.swift
+++ ios/Sources/KeychainPlugin.swift
@@ -17,10 +17,8 @@ struct KeychainResponse: Codable {
 class KeychainPlugin: Plugin {
   @objc public func getItem(_ invoke: Invoke) throws {
 		
-		NSLog("[KeychainPlugin]: getItem start")
     let args = try invoke.parseArgs(KeychainArgs.self)
 		let key = args.key
-		NSLog("[KeychainPlugin]: getItem \(key)")
 		let query = [
 				kSecClass: kSecClassGenericPassword,
 				kSecAttrAccount: key,
@@ -32,7 +30,6 @@ class KeychainPlugin: Plugin {
 		let status = SecItemCopyMatching(query, &data)
 		
 		guard status == errSecSuccess, let resultData = data as? Data else {
-			NSLog("[KeychainPlugin]: getItem error")
 			invoke.resolve(KeychainResponse(password: nil))
 			return
 		}
@@ -40,29 +37,24 @@ class KeychainPlugin: Plugin {
 		let password = String(data: resultData, encoding: .utf8)
 						
 		let resp = KeychainResponse(password: password)
-		NSLog("[KeychainPlugin]: getItem password: \(password)")
 		invoke.resolve(resp)
   }
 	
 	@objc public func saveItem(_ invoke: Invoke) throws {
-		NSLog("[KeychainPlugin]: saveItem start")
 	  let args = try invoke.parseArgs(KeychainArgs.self)
 		let key = args.key
 		let value = args.password ?? ""
 		guard let data = value.data(using: .utf8) else { 
-			NSLog("[KeychainPlugin]: saveItem convert data error")
 			invoke.resolve(false)
 			return 
 		}
 		        
-		NSLog("[KeychainPlugin]: saveItem delete before")
 		let query = [
 				kSecClass: kSecClassGenericPassword,
 				kSecAttrAccount: key
 		] as CFDictionary
 		SecItemDelete(query)
 		
-		NSLog("[KeychainPlugin]: saveItem add before")
 		let attributes = [
 				kSecClass: kSecClassGenericPassword,
 				kSecAttrAccount: key,
@@ -70,7 +62,6 @@ class KeychainPlugin: Plugin {
 		] as CFDictionary
 		let status = SecItemAdd(attributes, nil)
 		
-		NSLog("[KeychainPlugin]: saveItem done")
 		invoke.resolve(status == errSecSuccess)
 	}
 	@objc public func removeItem(_ invoke: Invoke) throws {
